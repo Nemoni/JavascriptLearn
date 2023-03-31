7dd4e6391ec368a10548723548daad7084fb000a
Nemoni <490202694@qq.com>
2023-03-31 16:23:43 +0800

    数据类型转换

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-import { prompt, confirm } from "./lesson1/interaction.js";
+import { dataConvert } from "./lesson1/dataConvert.js";
 
-// 调用prompt函数，等待用户输入，异步
-prompt('How old are you?', 100).then((age) => {
-    console.log(`Your age is ${age} years old.`);
-});
-
-// 等待用户输入，同步
-let money = await prompt('How much money do you have?', 2000);
-console.log(`Your have ${money} dollars.`);
-
-// 调用confirm函数，等待用户输入
-confirm('Are you the boss?').then((isBoss) => {
-  console.log(`You are ${isBoss ? '' : 'not '}the boss.`);
-});
-
-// 等待用户输入，同步
-let isBoy = await confirm('Are you a boy?');
-console.log(`You are ${isBoy ? '' : 'not '}a boy.`);
+dataConvert();
---
+++ lesson1/dataConvert.js
@@ -0,0 +1,35 @@
+// 类型转换
+function dataConvert(){
+    let name = "nemo";
+    let age = 3;
+    let str = "123r";
+    let x;
+
+    // 字符串
+    console.log("String:");
+    console.log(age); // 3, 自动转换成string
+    console.log(String(age)); // 3, 显示转换成string
+    // 数字型
+    console.log("Nubmer:");
+    console.log(Number(name)); // NaN
+    console.log(Number(x)); // NaN, 声明但未定义
+    console.log(Number(str)); // NaN
+    console.log(Number(true)); // 1
+    console.log(Number(null)); // 0
+    console.log(Number(undefined)); // NaN
+    let und = undefined;
+    console.log(Number(und)); // NaN
+    //console.log(Number(undefvar)); // ReferenceError，报错
+    // 布尔型
+    console.log("Boolean:");
+    console.log(Boolean(null)); // false
+    console.log(Boolean("")); // false
+    console.log(Boolean(0)); // false
+    console.log(Boolean(undefined)); // false
+    console.log(Boolean(" ")); // true
+    console.log(Boolean("0")); // true
+}
+
+export{
+    dataConvert,
+}
